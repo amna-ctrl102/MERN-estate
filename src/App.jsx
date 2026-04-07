94032de764eeaf152a0799a343154716bcf13291
amna-ctrl102 <rajpootamna393@gmail.com>
2026-04-07 16:55:27 +0500

    Create pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
 
 export default function App() {
   return (
     <div>
-      <h1 className="text-red-500"> Amna</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/sign-in" element={<SignIn/>}></Route>
+          <Route path="/sign-up" element={<SignUp/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
