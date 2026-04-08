3596e3299d23539d64f796c77261e108fc656d92
amna-ctrl102 <rajpootamna393@gmail.com>
2026-04-08 22:47:30 +0500

    Connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express= require("express");
+const mongoose=require("mongoose");
+const dotenv =require("dotenv");
 const app=express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to Mongodb")
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000,()=>{
     console.log("server is running");
-})
+});
