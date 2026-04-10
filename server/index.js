afa46b2dbfc7182b8ce2d4c048f8e94c5ee602e8
amna-ctrl102 <rajpootamna393@gmail.com>
2026-04-10 17:39:22 +0500

    Move package.json to Root and Create User Model

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express= require("express");
 const mongoose=require("mongoose");
-const dotenv =require("dotenv");
+require("dotenv").config();
 const app=express();
-dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to Mongodb")
