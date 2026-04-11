243a64d973a4c3ec4782f5874a048e205dbf1d15
amna-ctrl102 <rajpootamna393@gmail.com>
2026-04-11 14:59:38 +0500

    Create SignUp Api Route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express= require("express");
-const mongoose=require("mongoose");
+const mongoose= require("mongoose");
+const authRouter= require("./routes/auth.router.js");
 require("dotenv").config();
 const app=express();
 
@@ -8,6 +9,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err);
 })
+
+app.use(express.json());
+
+app.use("/api/auth",authRouter);
 
 app.listen(3000,()=>{
     console.log("server is running");
