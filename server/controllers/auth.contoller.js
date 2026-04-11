457944665840c9b7d20c0780118de9172c5c1933
amna-ctrl102 <rajpootamna393@gmail.com>
2026-04-11 16:56:59 +0500

    Create a Middleware And a Function To Handle The possible Errors

--- server/controllers/auth.contoller.js
+++ server/controllers/auth.contoller.js
@@ -1,6 +1,7 @@
 const User= require("../models/user.model.js")
 const bcryptjs=require("bcryptjs");
-const signup= async(req,res)=>{
+
+const signup= async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashedPassword= bcryptjs.hashSync(password,10);
     const newUser= new User({username,email,password:hashedPassword});
@@ -8,7 +9,7 @@ const signup= async(req,res)=>{
         await newUser.save();
         res.status(201).json("User created successfully");
     }catch(err){
-        res.status(500).json(err.errmsg);   
+        next(err);   
     }
 };
 
--- server/index.js
+++ server/index.js
@@ -14,6 +14,16 @@ app.use(express.json());
 
 app.use("/api/auth",authRouter);
 
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
+
 app.listen(3000,()=>{
     console.log("server is running");
 });
