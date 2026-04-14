1bd25c01ab610044a3ed389f28a8617fe1415c06
amna-ctrl102 <rajpootamna393@gmail.com>
2026-04-15 00:57:27 +0500

    Create Sign In Api Route

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -1,7 +1,8 @@
 const express= require("express");
-const { signup } = require("../controllers/auth.contoller.js");
+const { signup, signin } = require("../controllers/auth.contoller.js");
 const router= express.Router();
 
 router.post("/signup",signup);
+router.post("/signin",signin);
 
 module.exports= router;
