a6567fe83709d23a4db075fd940bbc55634cfb49
amna-ctrl102 <rajpootamna393@gmail.com>
2026-05-21 23:35:09 +0500

    Complete Delete User Listing Functionality

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 const Listing = require("../models/listing.model");
+const { errorHandler } = require("../utils/error");
 
 const createListing=async(req,res,next)=>{
     try{
@@ -9,6 +10,22 @@ const createListing=async(req,res,next)=>{
     }
 };
 
+const deleteListing=async(req,res,next)=>{
+    const listing= await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404,"Listing not found!"));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorhandler(401,"You can only delete your own listing"));
+    }
+    try{
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json("Listing has been deleted!")
+    }catch(error){
+        next(error);
+    }
+};
 module.exports={
     createListing,
+    deleteListing
 }
--- server/routes/listing.router.js
+++ server/routes/listing.router.js
@@ -1,8 +1,9 @@
 const express=require("express");
-const { createListing } = require("../controllers/listing.controller");
+const { createListing,deleteListing } = require("../controllers/listing.controller");
 const { verifyToken } = require("../utils/verifyUserToken");
 const router=express.Router();
 
 router.post("/create",verifyToken,createListing);
+router.delete("/delete/:id",verifyToken,deleteListing)
 
 module.exports=router;
